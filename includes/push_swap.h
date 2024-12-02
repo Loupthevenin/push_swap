f41710c23008d6f88315f8a049ad88f14624dac1
Loup Thevenin <loup.thevenin.1@gmail.com>
2024-12-02 20:09:08 +0100

    add atoi long + start sort turk

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:48:59 by ltheveni          #+#    #+#             */
-/*   Updated: 2024/11/30 13:23:30 by ltheveni         ###   ########.fr       */
+/*   Updated: 2024/12/02 10:39:24 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int		rule_rr(int *tab_a, int *tab_b, int size_tab_a, int size_tab_b);
 int		rule_reverse_rotate(int *tab, int size_tab);
 int		rule_rrr(int *tab_a, int *tab_b, int size_tab_a, int size_tab_b);
 
+int		check_duplicate(int *tab, int size);
+long	ft_atoi_long(char *s);
 void	sort_tab(int *tab_a, int size);
 
 #endif
